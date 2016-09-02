bb04395984f98fac1935b27c36d41f2d5e7ac081
cathylu10 <LuC3424@sduhsd.lan>
2016-09-02 13:39:03 -0700

    Added Files

---
+++ APLesson_01/AsciiArt1.java
@@ -0,0 +1,10 @@
+public class firstProg
+{
+	public static void main(String[]args)
+	{
+		System.out.println("        __  __      __");
+		System.out.println("       (    I    )  / _/");
+		System.out.println("__ (  >O<   )");
+		System.out.println("\\_ \\ (__I__)");
+	}
+}
---
+++ APLesson_01/AsciiArt2.java
@@ -0,0 +1,13 @@
+public class AsciiArt2
+{
+	public static void main(String[]args)
+	{
+		System.out.println("                __         __");
+		System.out.println("               /   \\~~~/   \\");
+		System.out.println("    ,------(        .  .        )");
+		System.out.println("   /           \\__        __/");
+		System.out.println("  /|               (\\     |(");
+		System.out.println(" ^\\     /__\\     /\\   | ");
+		System.out.println("    |__|     |__|  -\"");
+	}
+}

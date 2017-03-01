a1ba95c8f7c2ea2a7663f32794c6c6925007235a
cathylu10 <LuC3424@sduhsd.lan>
2017-03-01 13:57:17 -0800

    lab 14 added

---
+++ APLesson_14/GMC.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+public class GMC implements Location
+{
+	private double x, y; 
+	
+	public GMC()
+	{
+		x = y = 0;
+	}
+	
+	public GMC(double xcoor, double ycoor)
+	{
+		x = xcoor;
+		y = ycoor;
+	}
+	
+	public int getID()
+	{
+		return (int)(10000000 * Math.random()+1);
+	}
+	
+	public void move(double xcoor, double ycoor)
+	{
+		x = xcoor;
+		y = ycoor;
+	}
+	
+	public double[] getLoc()
+	{
+		double[] location = new double[2];
+		location[0] = x;
+		location[1] = y;
+		return location;
+	}
+}

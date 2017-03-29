dfbe8d72354a9f29f24e704a79b689230921cb08
cathylu10 <LuC3424@sduhsd.lan>
2017-03-29 13:36:30 -0700

    magpie part 2

--- MagpieLab/MagpieLabPt2/Magpie2.java
+++ MagpieLab/MagpieLabPt2/Magpie2.java
@@ -62,17 +62,113 @@ public class Magpie2
 			response = "Tell me more about your pet";		
 		}
 		
-		else if (findKeyword(statement, "robinette") >= 0)
+		else if (findKeyword(statement, "Robinette") >= 0)
 		{
 			response = "He sounds like a pretty dank teacher";
 		}
-		 
+		
+		// Responses which require transformations
+		else if (findKeyword(statement, "I want to", 0) >= 0)
+		{
+			response = transformIWantToStatement(statement);
+		}
+
+
 		else
 		{
-			response = getRandomResponse();
+			// Look for a two word (you <something> me)
+			// pattern
+			int psn = findKeyword(statement, "you", 0);
+
+
+			if (psn >= 0
+					&& findKeyword(statement, "me", psn) >= 0)
+			{
+				response = transformYouMeStatement(statement);
+			}
+			
+			else
+			{
+				response = getRandomResponse();
+			}
 		}
 		return response;
+		
 	}
+	
+		/**
+	* Take a statement with "I want to <something>." and transform it into
+	* "What would it mean to <something>?"
+	* @param statement the user statement, assumed to contain "I want to"
+	* @return the transformed statement
+	*/
+	private String transformIWantToStatement(String statement)
+	{
+			/**
+	   * trim the statement
+	   * variable lastChar = last character in statement
+	   * if lastChar is a period...
+	   *        remove the last character from statement
+	   *
+	   * Set new int psn to the result from...
+	   *        findKeyword() method @param statement, goal is "I want to "
+	   * Set new String restOfStatement to the rest of statement after the
+	   * "I want to ".
+	   * /
+	   * return "What would it mean to" + restOfStatement; **/
+	   
+	   statement = statement.trim();
+	   String lastChar = statement.substring(statement.length()-2, statement.length()-1);
+	   if (lastChar.equals("."))
+	   {
+		   statement = statement.substring(0, statement.length()-1);
+	   }
+	   
+	   int psn = findKeyword(statement, "I want to");
+	   String restOfStatement = statement.substring(psn + 9, statement.length());
+	   
+	   return "What would it mean to" + restOfStatement + "?";
+	}
+
+
+	/**
+	* Take a statement with "you <something> me" and transform it into
+	* "What makes you think that I <something> you?"
+	* @param statement the user statement, assumed to contain "you" followed by "me"
+	* @return the transformed statement
+	*/
+	private String transformYouMeStatement(String statement)
+	{
+	  /**
+	   * trim the statement
+	   * Set new String lastChar to the last character in statement
+	   * if lastChar is a period...
+	   *        remove the period
+	   *
+	   * Set new int psnOfYou to the result of findKeyword
+	   *        @param statement and "you"
+	   * Set new int psnOfMe to the result of findKeyword
+	   *      @param statement, "me", and psnOfYou + 3
+	   * Set new String restOfStatement to the rest of statement after "You" + 3,
+	   * and before "me".
+	   *
+	   * return "What makes you think that I" + restOfStatement + "you?"
+	   * */
+	   
+	   statement = statement.trim();
+	   String lastChar = statement.substring(statement.length()-2, statement.length()-1);
+	   if (lastChar.equals("."))
+	   {
+		   statement = statement.substring(0, statement.length()-1);
+	   }
+	   
+	   int psnOfYou = findKeyword(statement, "you");
+	   int psnOfMe = findKeyword(statement, "me", psnOfYou + 3);
+	   String restOfStatement = statement.substring(psnOfYou + 3, psnOfMe);
+	   
+	   return "What makes you think that I " + restOfStatement + "you?";
+	}
+	
 
 	/** Ex_02: The findKeyword() Method...
 	 * ========================================================= */
@@ -81,6 +177,7 @@ public class Magpie2
 		/* New String variable phrase = a more searchable version of statement.
 			-Use a combination of trim() and toLowerCase() modify statement. */
 		String phrase = statement.trim().toLowerCase();
+		goal = goal.toLowerCase();
 		
 		/*New int variable psn = the location of goal in phrase after
 			startPos */
